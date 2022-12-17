5a7de8fa3ec02994bd6cf2d1436dd51151ee51ff
rusakroman <rrusak87@gmail.com>
2022-12-17 16:57:12 +0300

    Завершили проект

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const burgerBtn = document.querySelector('.js-burger-btn');
+
+const navigation = document.querySelector('.js-navigation');
+
+const navigationClose = document.querySelector('.js-navigation-close');
+
+burgerBtn.addEventListener('click', () => {
+  navigation.style.right = '0';
+});
+
+navigationClose.addEventListener('click', () => {
+  navigation.style.right = '';
+});
